e80b49bdf455108ac42061d16aa2db89bf661fd1
mts-pac <mts.pacheco.sts@gmail.com>
2024-07-09 23:46:49 -0300

    add baseURL github pabes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/portfolio-nuxtjs/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   ssr: false,
   css: [
     '~/assets/scss/main.scss',
